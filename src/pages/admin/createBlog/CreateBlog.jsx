0de0512ce691f6021fd674990e1bf37b51dd1c6b
shubham Rawat <rawatshubam269@gmail.com>
2024-04-20 16:25:23 +0530

    Preview Section Not working so ruled out for time being

--- src/pages/admin/createBlog/CreateBlog.jsx
+++ src/pages/admin/createBlog/CreateBlog.jsx
@@ -16,9 +16,9 @@ function CreateBlog() {
   // console.log("text: ", text);
 
   // Create markup function
-  function createMarkup(c) {
-    return { __html: c };
-  }
+  // function createMarkup(c) {
+  //   return { __html: c };
+  // }
   return (
     <div className="container mx-auto max-w-5xl py-6">
       <div
@@ -134,88 +134,6 @@ function CreateBlog() {
         >
           Send
         </Button>
-
-        {/* Six Preview Section  */}
-        <div className="mt-2">
-          <h1 className=" text-center mb-3 text-2xl font-bold">Preview</h1>
-          <div className="content">
-            <div
-              className={`[&> h1]:text-[32px] [&>h1]:font-bold  [&>h1]:mb-2.5
-                        ${
-                          mode === "dark"
-                            ? "[&>h1]:text-[#ff4d4d]"
-                            : "[&>h1]:text-black"
-                        }
-
-                        [&>h2]:text-[24px] [&>h2]:font-bold [&>h2]:mb-2.5
-                        ${
-                          mode === "dark"
-                            ? "[&>h2]:text-white"
-                            : "[&>h2]:text-black"
-                        }
-
-                        [&>h3]:text-[18.72] [&>h3]:font-bold [&>h3]:mb-2.5
-                        ${
-                          mode === "dark"
-                            ? "[&>h3]:text-white"
-                            : "[&>h3]:text-black"
-                        }
-
-                        [&>h4]:text-[16px] [&>h4]:font-bold [&>h4]:mb-2.5
-                        ${
-                          mode === "dark"
-                            ? "[&>h4]:text-white"
-                            : "[&>h4]:text-black"
-                        }
-
-                        [&>h5]:text-[13.28px] [&>h5]:font-bold [&>h5]:mb-2.5
-                        ${
-                          mode === "dark"
-                            ? "[&>h5]:text-white"
-                            : "[&>h5]:text-black"
-                        }
-
-                        [&>h6]:text-[10px] [&>h6]:font-bold [&>h6]:mb-2.5
-                        ${
-                          mode === "dark"
-                            ? "[&>h6]:text-white"
-                            : "[&>h6]:text-black"
-                        }
-
-                        [&>p]:text-[16px] [&>p]:mb-1.5
-                        ${
-                          mode === "dark"
-                            ? "[&>p]:text-[#7efff5]"
-                            : "[&>p]:text-black"
-                        }
-
-                        [&>ul]:list-disc [&>ul]:mb-2
-                        ${
-                          mode === "dark"
-                            ? "[&>ul]:text-white"
-                            : "[&>ul]:text-black"
-                        }
-
-                        [&>ol]:list-decimal [&>li]:mb-10
-                        ${
-                          mode === "dark"
-                            ? "[&>ol]:text-white"
-                            : "[&>ol]:text-black"
-                        }
-
-                        [&>li]:list-decimal [&>ol]:mb-2
-                        ${
-                          mode === "dark"
-                            ? "[&>ol]:text-white"
-                            : "[&>ol]:text-black"
-                        }
-
-                        [&>img]:rounded-lg
-                        `}
-              dangerouslySetInnerHTML={createMarkup(blogs.content)}
-            ></div>
-          </div>
-        </div>
       </div>
     </div>
   );
