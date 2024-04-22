2c1b61e1654d03b773b15b89e9982854a6b0e288
shubham Rawat <rawatshubam269@gmail.com>
2024-04-22 11:57:55 +0530

    Search Functionality and navigate to blog added

--- src/components/searchDialog/SearchDialog.jsx
+++ src/components/searchDialog/SearchDialog.jsx
@@ -2,13 +2,15 @@ import { Fragment, useState } from "react";
 import { Dialog, DialogBody, Input } from "@material-tailwind/react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { useGlobalContext } from "../../hooks/useGlobalContext";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchDialog() {
   const [open, setOpen] = useState(false);
-
+  const navigate = useNavigate();
   const handleOpen = () => setOpen(!open);
 
-  const { mode } = useGlobalContext();
+  const { mode, searchkey, setSearchkey, getAllBlog } = useGlobalContext();
+
   return (
     <Fragment>
       {/* Search Icon  */}
@@ -32,6 +34,8 @@ export default function SearchDialog() {
             <Input
               color="white"
               type="search"
+              value={searchkey}
+              onChange={(e) => setSearchkey(e.target.value)}
               label="Type here..."
               className=" bg-orange-200"
               name="searchkey"
@@ -43,22 +47,31 @@ export default function SearchDialog() {
 
           {/* Blog Card  */}
           <div className="flex justify-center flex-wrap  sm:mx-auto sm:mb-2 -mx-2  mt-4 mb-2 ">
-            <div className="p-2 sm:w-1/4 w-full ">
-              <div className=" container mx-auto px-4 bg-gray-200 p-2 rounded-lg ">
-                {/* Blog Thumbnail  */}
-                <img
-                  className="w-20 mb-2 rounded-lg"
-                  src={"images/blog-card-1.jpg"}
-                  alt="blog-card-1"
-                />
-
-                {/* Blog Date  */}
-                <p className="w-40 text-sm">{"date"}</p>
-
-                {/* Blog Title  */}
-                <h1>{"title"}</h1>
-              </div>
-            </div>
+            {getAllBlog
+              .filter((obj) =>
+                obj.blogs.title.toLowerCase().includes(searchkey)
+              )
+              .map((item, index) => {
+                return (
+                  <div key={index} className="p-2 sm:w-1/4 w-full ">
+                    <div
+                      onClick={() => navigate(`/bloginfo/${item.id}`)}
+                      className=" container cursor-pointer mx-auto px-4 bg-gray-200 p-2 rounded-lg "
+                    >
+                      {/* Blog Thumbnail  */}
+                      <img
+                        className="w-20 mb-2 rounded-lg"
+                        src={item.thumbnail}
+                        alt=""
+                      />
+                      {/* Blog Date  */}
+                      <p className="w-40 text-sm">{item.date}</p>
+                      {/* Blog Title  */}
+                      <h1>{item.blogs.title}</h1>
+                    </div>
+                  </div>
+                );
+              })}
           </div>
         </DialogBody>
       </Dialog>
